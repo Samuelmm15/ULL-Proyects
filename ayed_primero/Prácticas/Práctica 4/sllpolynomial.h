33624671c45aeb5aceacc8f6a6e38aa543bdf660
Samuel <alu0101359526@ull.edu.es>
2021-04-18 12:25:03 +0100

    empezado

---
+++ ayed_primero/Prácticas/Práctica 4/sllpolynomial.h
@@ -0,0 +1,126 @@
+// AUTOR: Samuel Martín Morales
+// FECHA: 18/04/2021
+// EMAIL: alu0101359526@ull.edu.es
+// VERSION: 2.0
+// ASIGNATURA: Algoritmos y Estructuras de Datos
+// PRÁCTICA Nº: 4
+// ESTILO: Google C++ Style Guide
+// COMENTARIOS:
+// 
+
+#ifndef SLLPOLYNOMIAL_H_
+#define SLLPOLYNOMIAL_H_
+
+#include <iostream>
+#include <math.h>  // fabs, pow
+
+#include "pair_t.h"
+#include "sll_t.h"
+#include "vector_t.h"
+
+#define EPS 1.0e-6
+
+typedef pair_t<double> pair_double_t;  // Campo data_ de SllPolynomial
+typedef sll_node_t<pair_double_t> SllPolyNode;  // Nodos de SllPolynomial
+
+// Clase para polinomios basados en listas simples de pares
+class SllPolynomial : public sll_t<pair_double_t> { /// clase heredada
+ public:
+  // constructores
+  SllPolynomial(void) : sll_t() {};
+  SllPolynomial(const vector_t<double>&, const double = EPS);
+
+  // destructor
+  ~SllPolynomial() {};
+
+  // E/S
+  void Write(std::ostream& = std::cout) const;
+  
+  // operaciones
+  double Eval(const double) const; /// evaluación de un polinomio
+  bool IsEqual(const SllPolynomial&, const double = EPS) const; /// comprobación de si son iguales
+  void Sum(const SllPolynomial&, SllPolynomial&, const double = EPS); /// suma de dos polinomios
+};
+
+
+bool IsNotZero(const double val, const double eps = EPS) { /// comprobacion de si alguna celda del vector es igual a cero.
+  return fabs(val) > eps;
+};
+
+// FASE II
+// constructor
+SllPolynomial::SllPolynomial(const vector_t<double>& v, const double eps) { /// constructor del polinomio
+  pair_double_t aux;
+  SllPolyNode auxiliary;
+  int counter = 0;
+  for (int i = 0; i < v.get_size(); i++) {
+    if (IsNotZero(v.get_val(i), eps) == true) {
+      aux.set(v.get_val(i), i);
+      counter++;
+    }
+  }
+
+  for (int i = 0; i < counter; i++) {
+    
+  }
+};
+
+
+// E/S
+void SllPolynomial::Write(std::ostream& os) const {
+  os << "[ ";
+  bool first{true};
+  SllPolyNode* aux{get_head()};
+  while (aux != NULL) {
+    int inx{aux->get_data().get_inx()};
+    double val{aux->get_data().get_val()};
+    if (val > 0)
+      os << (!first ? " + " : "") << val;
+    else
+      os << (!first ? " - " : "-") << fabs(val);
+    os << (inx > 1 ? " x^" : (inx == 1) ? " x" : "");
+    if (inx > 1)
+      os << inx;
+    first = false;
+    aux = aux->get_next();
+  }
+  os << " ]" << std::endl;
+};
+
+std::ostream& operator<<(std::ostream& os, const SllPolynomial& p) {
+  p.Write(os);
+  return os;
+};
+
+
+// Operaciones con polinomios
+
+// FASE III
+// Evaluación de un polinomio representado por lista simple
+double SllPolynomial::Eval(const double x) const {
+  double result{0.0};
+  // poner el código aquí
+  
+  return result;
+};
+
+// Comparación si son iguales dos polinomios representados por listas simples
+bool SllPolynomial::IsEqual(const SllPolynomial& sllpol,
+			    const double eps) const {
+  bool differents = false;
+  // poner el código aquí
+
+  return !differents;
+};
+
+// FASE IV
+// Generar nuevo polinomio suma del polinomio invocante mas otro polinomio
+void SllPolynomial::Sum(const SllPolynomial& sllpol,
+			SllPolynomial& sllpolsum,
+			const double eps) {
+  // poner el código aquí
+
+};
+
+
+#endif  // SLLPOLYNOMIAL_H_
