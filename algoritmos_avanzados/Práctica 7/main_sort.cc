898228a2f8388ce563091ca68c1f04c43617b9d2
Samuel <alu0101359526@ull.edu.es>
2021-04-20 11:24:25 +0100

    terminado

--- algoritmos_avanzados/Práctica 7/main_sort.cc
+++ algoritmos_avanzados/Práctica 7/main_sort.cc
@@ -9,9 +9,246 @@
  * 
  */
 
-#include "insertion.h"
-#include "quicksort.h"
-#include "shellsort.h"
+/// This coments help me to understand the functions of the differents librarys
+#include <iostream> /// the main library of the program
+#include <fstream> /// the library to read and write text files
+#include <stdlib.h> /// the library to use exit(1);
+#include <string.h> /// the library to operate with strings
+#include <vector> /// the library to do a vector with strings
+#include <algorithm> /// the library to use the iterator
+#include <cctype> /// the library to use toupper or tolower
+#include <sstream> /// library to use the convertors of strings and numbers (istringstream or ostringstream)
+#include <list> /// library to use the class list
+
+using namespace std;
+
+template<typename Key> Key Vector_Sort_Insertion(vector<Key> &sec, Key number_cells);
+
+template<typename Key>
+Key Vector_Sort_Insertion(vector<Key> &sec, Key number_cells) { /// Fuction template for the method Insertion
+    cout << endl;
+    char ejecution_trace;
+    cout << "¿Prefiere mostrar en todo momento la traza de ejecución?" << endl;
+    cout << "[s/n]: ";
+    cin >> ejecution_trace;
+
+    if (ejecution_trace == 's') {
+        int x = 0;
+        int j = 0;
+        for (int i = 0; i < number_cells; i++) {
+            for (int k = 1; k < sec.size(); k++) {
+                j = k;
+                x = sec[k];
+                sec[-1] = x;
+                while ((x < sec[j-1]) && (j > 0)) {
+                    cout << endl;
+                    cout << "Antes de realizar el cambio: " << endl;
+                    cout << sec[j] << " y " << sec[j-1] << endl;
+                    sec[j] = sec[j-1];
+                    j--;
+
+                    cout << "En este punto tenemos el vector: " << endl;
+                    for (int i = 0; i < sec.size(); i++) {
+                        cout << sec.at(i) << " ";
+                    }
+                    cout << endl;
+                }
+                sec[j] = x;
+            }
+        }
+    } else if (ejecution_trace == 'n') {
+        int x = 0;
+        int j = 0;
+        for (int i = 0; i < number_cells; i++) {
+            for (int k = 1; k < sec.size(); k++) {
+                j = k;
+                x = sec[k];
+                sec[-1] = x;
+                while ((x < sec[j-1]) && (j > 0)) {
+                    sec[j] = sec[j-1];
+                    j--;
+                }
+                sec[j] = x;
+            }
+        }
+    }
+    
+    cout << endl;
+    cout << "El vector finalmente ordenado es: " << endl;
+    for (int i = 0; i < sec.size(); i++) {
+        cout << sec.at(i) << " ";
+    }
+    cout << endl;
+};
+
+template<typename Key>
+void Qsort(vector<Key> &sec, int ini, int fin, char ejecutation_trace) {
+    if (ejecutation_trace == 's') {
+        int i = ini;
+        int f = fin;
+
+        int p = sec[(i+f)/2];
+        while (i <= f) {
+            while (sec[i] < p) {
+                i++;
+            }
+            while (sec[f] > p) {
+                f--;
+            }
+            if (i <= f) {
+                cout << endl;
+                cout << "Antes de realizar el cambio: " << endl;
+                cout << sec[i] << " y " << sec[f] << endl;
+                swap(sec[i], sec[f]);
+                cout << "Tras el cambio: " << endl;
+                cout << sec[i] << " y " << sec[f] << endl;
+                i++;
+                f--;
+            }
+
+            cout << "En este punto tenemos el vector: " << endl;
+            for (int i = 0; i < sec.size(); i++) {
+            cout << sec.at(i) << " ";
+            }
+            cout << endl;
+        }
+        if (ini < f) {
+            Qsort(sec,ini,f,ejecutation_trace);
+        }
+        if (i < fin) {
+            Qsort(sec,i,fin,ejecutation_trace);
+        }
+    } else if (ejecutation_trace == 'n') {
+        int i = ini;
+        int f = fin;
+
+        int p = sec[(i+f)/2];
+        while (i <= f) {
+            while (sec[i] < p) {
+                i++;
+            }
+            while (sec[f] > p) {
+                f--;
+            }
+            if (i <= f) {
+                swap(sec[i], sec[f]);
+                i++;
+                f--;
+            }
+        }
+        if (ini < f) {
+            Qsort(sec,ini,f,ejecutation_trace);
+        }
+        if (i < fin) {
+            Qsort(sec,i,fin,ejecutation_trace);
+        }
+    }
+};
+
+template<typename Key> Key Vector_Sort_Quicksort(vector<Key> &sec, Key number_cells);
+
+template<typename Key>
+Key Vector_Sort_Quicksort(vector<Key> &sec, Key number_cells) { /// Fuction template for the method QuickSort
+    cout << endl;
+    char ejecution_trace;
+    cout << "¿Prefiere mostrar en todo momento la traza de ejecución?" << endl;
+    cout << "[s/n]: ";
+    cin >> ejecution_trace;
+
+    if (ejecution_trace == 's') {
+        int ini = 0;
+        int fin = number_cells - 1;
+
+        Qsort (sec,ini,fin, ejecution_trace);
+    } else if (ejecution_trace == 'n') {
+        int ini = 0;
+        int fin = number_cells - 1;
+
+        Qsort (sec,ini,fin, ejecution_trace);
+    }
+
+    cout << endl;
+    cout << "El vector finalmente ordenado es: " << endl;
+    for (int i = 0; i < sec.size(); i++) {
+        cout << sec.at(i) << " ";
+    }
+    cout << endl;
+};
+
+template<typename Key> Key Vector_Sort_Shellsort(vector<Key> &sec, Key number_cells);
+
+template<typename Key>
+Key Vector_Sort_Shellsort(vector<Key> &sec, Key number_cells) { /// Fuction template for the method ShellSort
+    cout << endl;
+    char ejecution_trace;
+    cout << "¿Prefiere mostrar en todo momento la traza de ejecución?" << endl;
+    cout << "[s/n]: ";
+    cin >> ejecution_trace;
+
+    if (ejecution_trace == 's') {
+        int del = number_cells;
+        while (del > 1) {
+            del = del / 2;
+
+            int d = 0;
+            d = del;
+            int n = 0;
+            n = number_cells;
+
+            for (int i = d; i < n; i++) {
+                int x;
+                x = sec[i];
+                int j;
+                j = i;
+                while ((j >= d) && (x < sec[j - d])) {
+                    cout << endl;
+                    cout << "Antes de realizar el cambio: " << endl;
+                    cout << sec[j] << " y " << sec[j - d] << endl;
+                    sec[j] = sec[j - d];
+                    cout << "Tras el cambio: " << endl;
+                    cout << sec[j] << " y " << sec[j - d] << endl;
+                    j = j - d;
+
+                    cout << "En este punto tenemos el vector: " << endl;
+                    for (int i = 0; i < sec.size(); i++) {
+                        cout << sec.at(i) << " ";
+                    }
+                    cout << endl;
+                }
+                sec[j] = x;
+            }
+        }
+    } else if (ejecution_trace == 'n') {
+        int del = number_cells;
+        while (del > 1) {
+            del = del / 2;
+
+            int d = 0;
+            d = del;
+            int n = 0;
+            n = number_cells;
+
+            for (int i = d; i < n; i++) {
+                int x;
+                x = sec[i];
+                int j;
+                j = i;
+                while ((j >= d) && (x < sec[j - d])) {
+                    sec[j] = sec[j - d];
+                    j = j - d;
+                }
+                sec[j] = x;
+            }
+        }
+    }
+    
+    cout << endl;
+    cout << "El vector finalmente ordenado es: " << endl;
+    for (int i = 0; i < sec.size(); i++) {
+        cout << sec.at(i) << " ";
+    }
+    cout << endl;
+};
 
 void clrscr() {
     system("clear");
@@ -34,23 +271,15 @@ int main() {
     cin >> cells_size;
 
     clrscr();
-    char ejecution_trace;
-    cout << "¿Prefiere mostrar en todo momento la traza de ejecución?" << endl;
-    cout << "[s/n]: ";
-    cin >> ejecution_trace;
-
-    clrscr();
     int write_option = 0;
     cout << "¿Prefiere introducir los valores que quiere ordenar, de manera manueal u aleatoria?" << endl;
     cout << "Si lo prefiere de manera manueal introduza [1], si lo prefiere aleatorio introduzca [2] : ";
     cin >> write_option;
 
     clrscr();
-    vector<int> vector_sort; 
-    Insertion<int> I;
+    vector<int> vector_sort;
 
-    if (write_option == 1) { /// en este punto comienza el programa
-        /// comenzamos con la creación del vector de valores
+    if (write_option == 1) { /// Here starts the programm
         int auxiliary = 0;
         cout << "Para comenzar la ordenación, introduzca los valores que quiere ordenar;" << endl;
         for (int i = 0; i < cells_size; i++) {
@@ -67,34 +296,21 @@ int main() {
         }
         cout << endl;
 
-        if (algorithm_option == 1) {  /// en esete punto es donde se producen las operaciones importantes
+        if (algorithm_option == 1) { 
 
-            if (ejecution_trace == 's') {
-                I.Insertion_Comparation(vector_sort, ejecution_trace);
-            } else if (ejecution_trace == 'n') {
-                I.Insertion_Comparation(vector_sort, ejecution_trace);
-            }
-
+            Vector_Sort_Insertion<int>(vector_sort, cells_size);
+        
         } else if (algorithm_option == 2) {
 
-            if (ejecution_trace == 's') {
-
-            } else if (ejecution_trace == 'n') {
-
-            }
+            Vector_Sort_Quicksort<int>(vector_sort, cells_size);
 
         } else if (algorithm_option == 3) {
 
-            if (ejecution_trace == 's') {
-
-            } else if (ejecution_trace == 'n') {
-
-            }
-
+            Vector_Sort_Shellsort<int>(vector_sort, cells_size);
+            
         }
 
     } else if (write_option == 2) {
-        /// Comenzamos con la creación del vector de valores
         int range_min = 1;
         int range_max = 1000;
         int counter = 0;
@@ -116,27 +332,15 @@ int main() {
 
         if (algorithm_option == 1) {
 
-            if (ejecution_trace == 's') {
-                I.Insertion_Comparation(vector_sort, ejecution_trace);
-            } else if (ejecution_trace == 'n') {
-                I.Insertion_Comparation(vector_sort, ejecution_trace);
-            }
+            Vector_Sort_Insertion<int>(vector_sort, cells_size);
 
         } else if (algorithm_option == 2) {
 
-            if (ejecution_trace == 's') {
-
-            } else if (ejecution_trace == 'n') {
-
-            }
-
+            Vector_Sort_Quicksort<int>(vector_sort, cells_size);
+            
         } else if (algorithm_option == 3) {
 
-            if (ejecution_trace == 's') {
-
-            } else if (ejecution_trace == 'n') {
-
-            }
+            Vector_Sort_Shellsort<int>(vector_sort, cells_size);
 
         }
 
