b276b944978ce5e45e6727f09a30f0e3513c1f47
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-03-12 19:09:27 +0000

    Advanced

--- Tercero/Practicas_SSI/P3/chacha20.py
+++ Tercero/Practicas_SSI/P3/chacha20.py
@@ -9,6 +9,7 @@
 """
 
 # Libraries declaration
+from itertools import count
 from msilib.schema import Binary
 import re
 import secrets
@@ -27,7 +28,7 @@ def Menu():
 def Aleatory_Nonce():
     number_of_bytes = 12 # This is the 96 bits or 12 bytes
     random_generator = secrets.token_hex(number_of_bytes) # This fuction generates aleatory tokens
-    print(random_generator)
+    # print(random_generator) # To comprobe
     
     i = 0
     counter = 0
@@ -42,72 +43,131 @@ def Aleatory_Nonce():
         counter += 1
         i += 1
 
-    print()
-    print(nonce_result) # To comprobe the result
+    # print()
+    # print(nonce_result) # To comprobe the result
     
     return nonce_result
     
-def ROTL(a_number, b_number):
-    return (((int(a_number)) << (int(b_number))) | ((int(a_number)) >> (32 - (int(b_number)))))
+def ROTL(a_number, b_number): # esta mierda no funciona nada bien
+    a_number = bin(int(a_number, 16))[2:]
+    number_b = str(b_number)
+    number_b = bin(int(number_b, 16))[2:]
+    number_substract = str('32')
+    number_substract = bin(int(number_substract, 16))[2:]
+    auxiliary_array = list(range(4))
+    auxiliary_array[0] = a_number
+    auxiliary_array[1] = number_b
+    auxiliary = bin(int(auxiliary_array[0], 2))
+    auxiliary_array[2] = bin(int(auxiliary,2)<<b_number)[2:]
+    auxiliary = bin(int(number_substract, 2) - int(auxiliary_array[1], 2))[2:]
+    auxiliary_array[3] = bin(int(auxiliary_array[0], 2)> 32 - b_number)[2:]
+    auxiliary_array[0] = bin(int(auxiliary_array[2], 2) | int(auxiliary_array[3], 2))[2:]
+    auxiliary = hex(int(auxiliary_array[0], 2))[2:]
+    print(auxiliary)
+    return auxiliary
     
-def QR(a_number, b_number, c_number, d_number):
-    QR_array = []
-    QR_array.append(a_number + b_number)
-    d_number = bin(int(d_number, 2) ^ int(a_number, 2))
-    QR_array.append(d_number[2:])
-    QR_array.append(ROTL(d_number[2:], 16))
     
-    QR_array.append(c_number + d_number[2:])
-    b_number = bin(int(b_number, 2) ^ int(c_number, 2))
-    QR_array.append(b_number[2:])
-    QR_array.append(ROTL(b_number[2:], 12))
+def QR(a_number, b_number, c_number, d_number, QR_array):
+    QR_array[a_number] = hex(int(QR_array[a_number], 16) + int(QR_array[b_number], 16))[2:]
+    QR_array[d_number] = hex(int(QR_array[d_number], 16) ^ int(QR_array[a_number], 16))[2:]
+    QR_array[d_number] = ROTL(QR_array[d_number], 16)
     
-    QR_array.append(a_number + b_number[2:])
-    d_number = bin(int(d_number, 2) ^ int(a_number, 2))
-    QR_array.append(d_number[2:])
-    QR_array.append(ROTL(d_number[2:], 8))
+    QR_array[c_number] = hex(int(QR_array[c_number], 16) + int(QR_array[d_number], 16))[2:]
+    QR_array[b_number] = hex(int(QR_array[b_number], 16) ^ int(QR_array[c_number], 16))[2:]
+    QR_array[b_number] = ROTL(QR_array[b_number], 12)
     
-    QR_array.append(c_number + d_number[2:])
-    b_number = bin(int(b_number, 2) ^ int(c_number, 2))
-    QR_array.append(b_number[2:])
-    QR_array.append(ROTL(b_number[2:], 7))
+    QR_array[a_number] = hex(int(QR_array[a_number], 16) + int(QR_array[b_number], 16))[2:]
+    QR_array[d_number] = hex(int(QR_array[d_number], 16) ^ int(QR_array[a_number], 16))[2:]
+    QR_array[d_number] = ROTL(QR_array[d_number], 8)
     
-    return QR_array
+    QR_array[c_number] = hex(int(QR_array[c_number], 16) + int(QR_array[d_number], 16))[2:]
+    QR_array[b_number] = hex(int(QR_array[b_number], 16) ^ int(QR_array[c_number], 16))[2:]
+    QR_array[b_number] = ROTL(QR_array[b_number], 7)
     
 def Chacha20_Encryption(hexadecimal_key, hexadecimal_counter, hexadecimal_nonce):
-    ROUNDS = 20 # This is the const of the number of rounds
-    result = [];
+    S_initial = list(range(16))
     
-    i = 0
-    binary_key = format(int(hexadecimal_key[i]), "08b")
-    while i < 23:
-        if hexadecimal_key[i] != ':':
-            binary_key = format(int(hexadecimal_key[i]), "08b")
-            result.append(binary_key)
+    # Constat initialization
+    S_initial[0] = '61707865'
+    S_initial[1] = '3320646e'
+    S_initial[2] = '79622d32'
+    S_initial[3] = '6b206574'
+    
+    # Key initialization
+    hexadecimal_key = str(hexadecimal_key)
+    hexadecimal_key = hexadecimal_key.replace(':', '')
+    k = 4
+    l = 0
+    counter = 0
+    aux_string = ''
+    while k < 12:
+        while counter != 8:
+            aux_string = aux_string + hexadecimal_key[l]
+            counter += 1
+            l += 1
+        S_initial[k] = aux_string
+        # print(S_initial[k]) # To comprobe
+        aux_string = ''
+        k += 1
+        counter = 0
+    
+    # Counter initialization
+    hexadecimal_counter = str(hexadecimal_counter)
+    hexadecimal_counter = hexadecimal_counter.replace(':', '')
+    S_initial[12] = hexadecimal_counter
+    
+    # Nonce initilization
+    hexadecimal_nonce = str(hexadecimal_nonce)
+    hexadecimal_nonce = hexadecimal_nonce.replace(':', '')
+    k = 13
+    l = 0
+    counter = 0
+    aux_string = ''
+    while k < 16:
+        while counter != 8:
+            aux_string = aux_string + hexadecimal_nonce[l]
+            counter += 1
+            l += 1
+        S_initial[k] = aux_string
+        aux_string = ''
+        k += 1
+        counter = 0
         
+    print()
+    print('ESTADO INICIAL= ')
+    print(S_initial) # To comprobe
+    
+    ROUNDS = 20 # This is the const of the number of rounds
+    S_result = [];
+    
+    # Binary transformation
+    i = 0
+    while i < 16:
+        # auxiliary_S = bin(int(S_initial[i], 16))[2:].zfill(32) # with the 16, the hexadecimal number is converted into decimal, and the, the decimal number into binary
+        S_result.append(S_initial[i]) # with zfill, the binary number is refill with zeros
         i += 1
 
-    print(result) # Necesary to comprobe
-    print
+    # print()
+    # print(S_result) # Necesary to comprobe
+    # print
     
+    # Interactions to transform
     i = 0
     while i < ROUNDS:
-        print(QR(result[0], result[4], result[8], result[12]))
-        print()
-        print(QR(result[1], result[5], result[9], result[13]))
-        print()
-        print(QR(result[2], result[6], result[11], result[15]))
-        print()
-        
-        print(QR(result[0], result[5], result[10], result[15]))
-        print()
-        print(QR(result[1], result[6], result[11], result[12]))
-        print()
-        print(QR(result[2], result[7], result[8], result[13]))
-        print()
-        print(QR(result[3], result[4], result[9], result[14]))
-        print()
+        # Column Round
+        QR(0, 4, 8, 12, S_result); # columna 0
+        QR(1, 5, 9, 13, S_result); # columna 1
+        QR(2, 6, 10, 14, S_result); # columna 2
+        QR(3, 7, 11, 15, S_result); # columna 3
+        # Diagonal Round 
+        QR(0, 5, 10, 15, S_result); # diagonal 1
+        QR(1, 6, 11, 12, S_result); # diagonal 2
+        QR(2, 7, 8, 13, S_result); # diagonal 3
+        QR(3, 4, 9, 14, S_result); # diagonal 4
         i += 2
+    
+    print('ESTADO FINAL TRAS LAS 20 INTERACCIONES: ')
+    print(S_result)
           
     # EN ESTE PUNTO FALTA EL ÚLTIMO BUCLE FOR DEL ALGORITMO DE LOS APUNTES DE LA ASIGNATURA
 
@@ -119,3 +179,7 @@ if __name__ == '__main__':
 
 # Generador de tokens aleatorios de cualquier tipo
 # https://docs.python.org/es/3.10/library/secrets.html#secrets.token_bytes
+# Conversor de hexadecimal a binario
+# https://www.delftstack.com/es/howto/python/python-hex-to-int/
+# Binary numbers
+# https://www.techiedelight.com/how-to-convert-an-integer-to-a-binary-string-in-python/#:~:text=format()%20function,the%20string%20presentation%20type%20b%20.
