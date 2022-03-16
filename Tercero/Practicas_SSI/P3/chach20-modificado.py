e7f3e5a46bbef5f38b68e15232e320312ab7fa11
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-03-16 09:41:47 +0000

    Finished

---
+++ Tercero/Practicas_SSI/P3/chach20-modificado.py
@@ -0,0 +1,204 @@
+""""
+ @file chacha20.py
+ @author Samuel Martín Morales (alu0101359526@ull.edu.es)
+ @brief Implementation of Chacha20 encryption
+ @version 0.1
+ @date 2022-03-10
+  
+ @copyright Copyright (c) 2022
+"""
+
+# Libraries declaration
+import secrets
+# Function declaration
+
+def Menu():
+    print("<< Bienvenido al programa de cifrado Chacha20 >>")
+    print()
+    
+    hexadecimal_key = input("Introduzca la clave de cifrado: ")
+    hexadecimal_counter = input("Introduzca el contador: ")
+    hexadecimal_nonce = Aleatory_Nonce()
+    
+    Chacha20_Encryption(hexadecimal_key, hexadecimal_counter, hexadecimal_nonce)
+    
+def Aleatory_Nonce():
+    number_of_bytes = 12 # This is the 96 bits or 12 bytes
+    random_generator = secrets.token_hex(number_of_bytes) # This fuction generates aleatory tokens
+    # print(random_generator) # To comprobe
+    
+    i = 0
+    counter = 0
+    nonce_result = ""
+    while i != len(random_generator):
+        if counter == 2:
+            nonce_result = nonce_result + ":"
+            nonce_result = nonce_result + random_generator[i]
+            counter = 0
+        else:
+            nonce_result = nonce_result + random_generator[i]
+        counter += 1
+        i += 1
+    # nonce_result = '00:00:00:09:00:00:00:4a:00:00:00:00'
+    print()
+    print('El noncce generado de manera aleatoria es: ')
+    print(nonce_result) # To comprobe the result
+    return nonce_result
+    
+def ROTL(a_number, b_number):
+    a_number = int(a_number, 16)
+    auxiliary = (((a_number) << (b_number)) | ((a_number) >> (32 - (b_number))))
+    auxiliary = str(auxiliary)
+    auxiliary = hex(int(hex(int(auxiliary)),16) & 0xFFFFFFFF)[2:]
+    return auxiliary
+    
+def QR(a_number, b_number, c_number, d_number, QR_array):
+    QR_array[a_number] = hex(int(hex(int(QR_array[a_number], 16) + int(QR_array[b_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[d_number] = hex(int(hex(int(QR_array[d_number], 16) ^ int(QR_array[a_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[d_number] = hex(int(ROTL(QR_array[d_number], 16), 16) & 0xFFFFFFFF)[2:]
+    
+    QR_array[c_number] = hex(int(hex(int(QR_array[c_number], 16) + int(QR_array[d_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[b_number] = hex(int(hex(int(QR_array[b_number], 16) ^ int(QR_array[c_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[b_number] = hex(int(ROTL(QR_array[b_number], 12), 16) & 0xFFFFFFFF)[2:]
+   
+    QR_array[a_number] = hex(int(hex(int(QR_array[a_number], 16) + int(QR_array[b_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[d_number] = hex(int(hex(int(QR_array[d_number], 16) ^ int(QR_array[a_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[d_number] = hex(int(ROTL(QR_array[d_number], 8), 16) & 0xFFFFFFFF)[2:]
+    
+    QR_array[c_number] = hex(int(hex(int(QR_array[c_number], 16) + int(QR_array[d_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[b_number] = hex(int(hex(int(QR_array[b_number], 16) ^ int(QR_array[c_number], 16)), 16) & 0xFFFFFFFF)[2:]
+    QR_array[b_number] = hex(int(ROTL(QR_array[b_number], 7), 16) & 0xFFFFFFFF)[2:]
+    
+def Chacha20_Encryption(hexadecimal_key, hexadecimal_counter, hexadecimal_nonce):
+    S_initial = list(range(16))
+    
+    # Constat initialization
+    S_initial[0] = '61707865'
+    S_initial[1] = '3320646e'
+    S_initial[2] = '79622d32'
+    S_initial[3] = '6b206574'
+    
+    # Key initialization
+    hexadecimal_key = str(hexadecimal_key)
+    hexadecimal_key = hexadecimal_key.replace(':', '')
+    k = 4
+    l = 0
+    counter = 0
+    aux_string = ''
+    while k < 12:
+        while counter != 8:
+            aux_string = aux_string + hexadecimal_key[l]
+            counter += 1
+            l += 1
+        S_initial[k] = aux_string
+        aux_string = ''
+        k += 1
+        counter = 0
+    
+    # Counter initialization
+    hexadecimal_counter = str(hexadecimal_counter)
+    hexadecimal_counter = hexadecimal_counter.replace(':', '')
+    S_initial[12] = hexadecimal_counter
+    
+    # Nonce initilization
+    hexadecimal_nonce = str(hexadecimal_nonce)
+    hexadecimal_nonce = hexadecimal_nonce.replace(':', '')
+    k = 13
+    l = 0
+    counter = 0
+    aux_string = ''
+    while k < 16:
+        while counter != 8:
+            aux_string = aux_string + hexadecimal_nonce[l]
+            counter += 1
+            l += 1
+        S_initial[k] = aux_string
+        aux_string = ''
+        k += 1
+        counter = 0
+        
+    # Little Endian convertion
+    i = 4
+    while i < 16:
+        auxiliary = bytearray.fromhex(S_initial[i])
+        auxiliary.reverse()
+        auxiliary = ''.join(format(x, '02x') for x in auxiliary)
+        S_initial[i] = auxiliary
+        i += 1
+    
+    print()
+    print('ESTADO INICIAL= ')
+    counter = 0 
+    for line in S_initial:
+        print(line, end=" ") # Neccesary to modificate the end, and prints, the matrix at the correct form
+        counter += 1
+        if counter == 4:
+            print()
+            counter = 0
+    
+    ROUNDS = 20 # This is the const of the number of rounds
+    S_result = [];
+    
+    # List Copy
+    i = 0
+    while i < 16:
+        S_result.append(S_initial[i])
+        i += 1
+    
+    # Interactions to transform
+    i = 0
+    while i < ROUNDS:
+        # Column Round
+        QR(0, 4, 8, 12, S_result); # column 0
+        QR(1, 5, 9, 13, S_result); # column 1
+        QR(2, 6, 10, 14, S_result); # column 2
+        QR(3, 7, 11, 15, S_result); # column 3
+        # Diagonal Round 
+        QR(0, 5, 10, 15, S_result); # diagonal 1
+        QR(1, 6, 11, 12, S_result); # diagonal 2
+        QR(2, 7, 8, 13, S_result); # diagonal 3
+        QR(3, 4, 9, 14, S_result); # diagonal 4
+        i += 2
+    
+    print()
+    print('ESTADO FINAL TRAS LAS 20 INTERACCIONES: ')
+    counter = 0 
+    for line in S_result:
+        print(line, end=" ") # Neccesary to modificate the end, and prints, the matrix at the correct form
+        counter += 1
+        if counter == 4:
+            print()
+            counter = 0
+          
+    i = 0
+    k = 0
+    l = 0
+    auxiliary_while = ''
+    while i < 16 :
+        auxiliary = hex(int(S_initial[i], 16) + int(S_result[i], 16))[2:]
+        auxiliary = str(auxiliary)
+        while l < 8:
+            auxiliary_while = auxiliary_while + auxiliary[l]
+            l += 1
+        auxiliary = auxiliary_while
+        l = 0
+        auxiliary_while = ''
+        S_result[i] = auxiliary
+        i += 1
+
+    print()
+    print('ESTADO DE SALIDA DEL GENERADOR: ')
+    counter = 0 
+    for line in S_result:
+        print(line, end=" ") # Neccesary to modificate the end, and prints, the matrix at the correct form
+        counter += 1
+        if counter == 4:
+            print()
+            counter = 0
+
+# Main Function
+if __name__ == '__main__':
+    Menu()
+
+# Key to introduce:
+# 00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13:14:15:16:17:18:19:1a:1b:1c:1d:1e:1f
