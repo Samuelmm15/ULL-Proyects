76b57a3d0dc52cd0c613f7e120ab42d92c1fe048
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-03-16 11:51:26 +0000

    Started

---
+++ Tercero/Practicas_SSI/P4/gps_c-a.py
@@ -0,0 +1,99 @@
+""""
+ @file gps_c-a.py
+ @author Samuel Martín Morales (alu0101359526@ull.edu.es)
+ @brief Implementation of C/A GPS encryption
+ @version 0.1
+ @date 2022-03-16
+  
+ @copyright Copyright (c) 2022
+"""
+
+# Library declaration
+
+# Function declaration
+def Menu():
+    print('<< BIENVENIDO A C/A GPS >>')
+    print()
+    
+    satellite_ID = input('Introduzca el ID del satélite el cual quiere hacer uso: ')
+    print()
+    exit_long = input('Introduzca la longitud de la secuencia de salida: ')
+    
+    print()
+    print(f'El ID del satélite seleccionado es {satellite_ID} y la longitud seleccionada de la secuencia de salida es {exit_long}')
+    final_satellite_ID = value_table(satellite_ID)
+    gps_c_a_encryption(satellite_ID, exit_long)
+    
+def value_table(satellite_ID):
+    final_satellite_ID = []
+    if satellite_ID == 1:
+        final_satellite_ID = [2, 6]
+    elif satellite_ID == 2:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 3:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 4:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 5:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 6:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 7:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 8:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 9:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 10:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 11:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 12:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 13:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 14:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 15:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 16:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 17:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 18:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 19:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 20:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 21:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 22:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 23:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 24:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 25:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 26:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 27:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 28:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 29:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 30:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 31:
+        final_satellite_ID = [3, 7]
+    elif satellite_ID == 32:
+        final_satellite_ID = [3, 7]
+def gps_c_a_encryption(satellite_ID, exit_long):
+    print() # La incialización de valores, siempre, es un vector de todo 1
+
+
+# Main functio
+if __name__ == '__main__':
+    Menu()
