209e54ef08538567740059c786ab84c049502683
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-03-18 19:07:56 +0000

    Advanced

--- Tercero/Practicas_SSI/P4/gps_c-a.py
+++ Tercero/Practicas_SSI/P4/gps_c-a.py
@@ -12,6 +12,10 @@
 
 # Function declaration
 
+from os import lseek
+from typing import final
+
+
 def Menu():
     print('<< BIENVENIDO A C/A GPS >>')
     print()
@@ -91,25 +95,70 @@ def value_table(satellite_ID):
     elif satellite_ID == '32':
         final_satellite_ID = [4, 9]
     return final_satellite_ID
+def gps_c_a_table(LFSR1, LFSR2, feedback_LFSR1, feedback_LFSR2, C_A_code):
+    print(LFSR1, end='         ')
+    print(feedback_LFSR1, end='           ')
+    print(LFSR2, end='         ')
+    print(feedback_LFSR2)
+
 def gps_c_a_encryption(satellite_ID, exit_long):
-    LFSR1_array = []
-    LFSR2_array = []
     final_taps = []
     
     # Initialization
-    i = 0
-    while i < 10:
-        LFSR1_array.append('1')
-        LFSR2_array.append('1')
-        i += 1
+    LFSR1 = '1111111111'
+    LFSR2 = '1111111111'
     final_taps = value_table(satellite_ID)
     
     print()
     print('De manera inicial se tiene: ')
-    print(f'LFSR1: {LFSR1_array}')
-    print(f'LFSR2: {LFSR2_array}')
+    print(f'LFSR1: {LFSR1}')
+    print(f'LFSR2: {LFSR2}')
     print(f'G2 taps: {final_taps}')
     
+    i = 0
+    first_position = 0
+    second_position = 0
+    last_position = 9
+    LFSR1_operator = 0
+    LFSR2_operator = 0
+    feedback_LFSR1 = 0
+    feedback_LFSR2 = 0
+    C_A_secuence = []
+    print()
+    print('LFSR1', end='          ')
+    print('realimentación', end='  ')
+    print('LFSR2', end='          ')
+    print('realimentación', end='   ')
+    print('Secuencia C/A PRN1')
+    while i < int(exit_long):
+        LFSR1_tap1 = 3
+        LFSR1_tap2 = 10
+        LFSR2_tap1 = final_taps[0]
+        LFSR2_tap2 = final_taps[1]
+        # LFSR1 operation
+        first_position = LFSR1[LFSR1_tap1 - 1]
+        second_position = LFSR1[LFSR1_tap2 - 1]
+        feedback_LFSR1 = bin(int(first_position, 2) ^ int(second_position, 2))[2:]
+        # scrolling
+        LFSR1 = feedback_LFSR1 + str(LFSR1)
+        LFSR1 = bin((int(LFSR1, 2) >> 1))[2:].zfill(10)
+        # LFSR2 operation
+        first_position = LFSR2[LFSR2_tap1 - 1]
+        second_position = LFSR2[LFSR2_tap2 - 1]
+        first_xor = bin(int(LFSR2[9], 2) ^ int(LFSR2[8], 2) ^ int(LFSR2[7], 2) ^ int(LFSR2[5], 2))[2:]
+        second_xor = bin(int(LFSR2[1], 2) ^ int(LFSR2[2], 2) ^ int(first_xor, 2))
+        feedback_LFSR2 = bin(int(second_xor, 2))[2:]
+        # scrolling
+        LFSR2 = feedback_LFSR2 + str(LFSR2)
+        LFSR2 = bin(int(LFSR2, 2) >> 1)[2:].zfill(10)
+        # C/A Code
+        LFSR1_operator = LFSR1[last_position]
+        LFSR2_operator = feedback_LFSR2
+        auxiliary = bin(int(LFSR1_operator, 2) ^ int(LFSR2_operator, 2))[2:]
+        C_A_secuence.append(auxiliary)
+        gps_c_a_table(LFSR1, LFSR2, feedback_LFSR1, feedback_LFSR2, auxiliary)
+        i += 1
+        
 
 
 # Main function
