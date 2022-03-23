cf49fe037173d27b2afa189823f70a84d83c6b3d
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-03-23 10:14:51 +0000

    Finished

--- Tercero/Practicas_SSI/P4/gps_c-a_modificado.py
+++ Tercero/Practicas_SSI/P4/gps_c-a_modificado.py
@@ -16,13 +16,16 @@ def Menu():
     print('<< BIENVENIDO A C/A GPS >>')
     print()
     
-    satellite_ID = input('Introduzca el ID del satélite el cual quiere hacer uso: ')
+    satellite_ID1 = input('Introduzca el ID del primer satélite el cual quiere hacer uso: ')
+    print()
+    satellite_ID2 = input('Introduzca el ID del segundo satélite el cual quiere hacer uso: ')
     print()
     exit_long = input('Introduzca la longitud de la secuencia de salida: ')
     
     print()
-    print(f'El ID del satélite seleccionado es {satellite_ID} y la longitud seleccionada de la secuencia de salida es {exit_long}')
-    gps_c_a_encryption(satellite_ID, exit_long)
+    print(f'El ID del primer satélite seleccionado es {satellite_ID1} y la longitud seleccionada de la secuencia de salida es {exit_long}')
+    print(f'El ID del primer satélite seleccionado es {satellite_ID2} y la longitud seleccionada de la secuencia de salida es {exit_long}')
+    gps_c_a_encryption(satellite_ID1, satellite_ID2, exit_long)
     
 def value_table(satellite_ID):
     final_satellite_ID = list(range(2))
@@ -98,19 +101,21 @@ def gps_c_a_table(LFSR1, LFSR2, feedback_LFSR1, feedback_LFSR2, C_A_code):
     print(feedback_LFSR2, end='                 ')
     print(C_A_code)
 
-def gps_c_a_encryption(satellite_ID, exit_long):
+def gps_c_a_encryption(satellite_ID1, satellite_ID2, exit_long):
     final_taps = []
     
     # Initialization
     LFSR1 = '1111111111'
     LFSR2 = '1111111111'
-    final_taps = value_table(satellite_ID)
+    final_taps1 = value_table(satellite_ID1)
+    final_taps2 = value_table(satellite_ID2)
     
     print()
     print('De manera inicial se tiene: ')
     print(f'LFSR1: {LFSR1}')
     print(f'LFSR2: {LFSR2}')
-    print(f'G2 taps: {final_taps}')
+    print(f'G1 taps: {final_taps1}')
+    print(f'G2 taps: {final_taps2}')
     
     i = 0
     first_position = 0
@@ -126,21 +131,36 @@ def gps_c_a_encryption(satellite_ID, exit_long):
     print('LFSR2', end='          ')
     print('realimentación', end='   ')
     print('Secuencia C/A PRN1')
-    feedback_LFSR1 = '0'
-    feedback_LFSR2 = '0'
-    auxiliary = 1
+    
+    # First Line
+    LFSR1_tap1 = final_taps1[0]
+    LFSR1_tap2 = final_taps1[1]
+    LFSR2_tap1 = final_taps2[0]
+    LFSR2_tap2 = final_taps2[1]
+    # LFSR1 operation
+    feedback_LFSR1 = bin(int(LFSR1[9], 2) ^ int(LFSR1[2], 2))[2:]
+    # LFSR2 operation
+    first_xor = bin(int(LFSR2[9], 2) ^ int(LFSR2[8], 2) ^ int(LFSR2[7], 2) ^ int(LFSR2[5], 2))[2:]
+    second_xor = bin(int(LFSR2[1], 2) ^ int(LFSR2[2], 2) ^ int(first_xor, 2))
+    feedback_LFSR2 = bin(int(second_xor, 2))[2:]
+    # C/A Code
+    LFSR1_operator = bin(int(LFSR1[LFSR1_tap1 - 1], 2) ^ int( LFSR1[LFSR1_tap2 - 1], 2))[2:]
+    LFSR2_operator = bin(int(LFSR2[LFSR2_tap1 - 1], 2) ^ int( LFSR2[LFSR2_tap2 - 1], 2))[2:]
+    auxiliary = bin(int(LFSR1_operator, 2) ^ int(LFSR2_operator, 2))[2:]
+    C_A_secuence.append(auxiliary)
     gps_c_a_table(LFSR1, LFSR2, feedback_LFSR1, feedback_LFSR2, auxiliary)
+    
     while i < (int(exit_long) - 1):
-        LFSR1_tap1 = 3
-        LFSR1_tap2 = 10
-        LFSR2_tap1 = final_taps[0]
-        LFSR2_tap2 = final_taps[1]
+        LFSR1_tap1 = final_taps1[0]
+        LFSR1_tap2 = final_taps1[1]
+        LFSR2_tap1 = final_taps2[0]
+        LFSR2_tap2 = final_taps2[1]
         # scrolling
         LFSR1 = feedback_LFSR1 + str(LFSR1)
         LFSR1 = bin((int(LFSR1, 2) >> 1))[2:].zfill(10)
         # LFSR1 operation
-        feedback_LFSR1 = bin(int(LFSR1[LFSR1_tap1 - 1], 2) ^ int(LFSR1[LFSR1_tap2 - 1], 2))[2:]
-         # scrolling
+        feedback_LFSR1 = bin(int(LFSR1[9], 2) ^ int(LFSR1[2], 2))[2:]
+        # scrolling
         LFSR2 = feedback_LFSR2 + str(LFSR2)
         LFSR2 = bin(int(LFSR2, 2) >> 1)[2:].zfill(10)
         # LFSR2 operation
@@ -148,7 +168,7 @@ def gps_c_a_encryption(satellite_ID, exit_long):
         second_xor = bin(int(LFSR2[1], 2) ^ int(LFSR2[2], 2) ^ int(first_xor, 2))
         feedback_LFSR2 = bin(int(second_xor, 2))[2:]
         # C/A Code
-        LFSR1_operator = LFSR1[9]
+        LFSR1_operator = bin(int(LFSR1[LFSR1_tap1 - 1], 2) ^ int( LFSR1[LFSR1_tap2 - 1], 2))[2:]
         LFSR2_operator = bin(int(LFSR2[LFSR2_tap1 - 1], 2) ^ int( LFSR2[LFSR2_tap2 - 1], 2))[2:]
         auxiliary = bin(int(LFSR1_operator, 2) ^ int(LFSR2_operator, 2))[2:]
         C_A_secuence.append(auxiliary)
