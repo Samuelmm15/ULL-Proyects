f276491e36c141fe206a9aa71152d23dad182f80
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-03-30 11:29:21 +0100

    Advanced

--- Tercero/Practicas_SSI/P6/aes_Rijndael_algorithm_addRoundKey.py
+++ Tercero/Practicas_SSI/P6/aes_Rijndael_algorithm_addRoundKey.py
@@ -9,4 +9,30 @@
 """
 
 def addRoundKey(key, text_block):
+    # Separation of the hexadecimal string into matrix
+    key_matrix = []
+    text_block_matrix = []
+    
+    i = 0
+    j = 0
+    counter = 0
+    auxiliary_string_key = ''
+    auxiliary_string_matrix = ''
+    while i < 32: # This is the divition
+        j = i
+        while counter < 2:
+            auxiliary_string_key = auxiliary_string_key + key[j]
+            auxiliary_string_matrix = auxiliary_string_matrix + text_block[j]
+            counter += 1
+            j += 1
+        key_matrix.append(auxiliary_string_key)
+        text_block_matrix.append(auxiliary_string_matrix)
+        auxiliary_string_matrix = ''
+        auxiliary_string_key = ''
+        counter = 0
+        i += 2
+    
     print()
+    print('Los valores separados por pares son: ')
+    print(key_matrix)
+    print(text_block_matrix)
