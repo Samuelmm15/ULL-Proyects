69d7bc6df5ec6b17d9d997e61a330d28589b2d0e
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-04-04 13:00:34 +0100

    MixColumn operation implemented

--- Tercero/Practicas_SSI/P6/aes_Rijndael_algorithm_MixColumn.py
+++ Tercero/Practicas_SSI/P6/aes_Rijndael_algorithm_MixColumn.py
@@ -17,55 +17,115 @@ def MixColumn(cipher_text_block):
                         ['3','1','1','2']]
     
     first_column = []
-    first_column_result = []
     second_column = []
-    second_column_result = []
-    thirth_column = []
     thirth_column = []
     fourth_column = []
-    fourth_column = []
-    
+        
     # Obtain the first column
     i = 0
     j = 0
     while i < len(cipher_text_block):
         first_column.append(cipher_text_block[i][j])
         i += 1
+    
+    c = 0
+    a = []
+    b = []
+    while c < 4:
+        a.append(hex(int(first_column[c],16)))
+        h = hex(int(first_column[c],16) & 0x80)
+        b.append(hex((int(first_column[c],16) << 1) % 256))
+        if (h == 0x80):
+            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
+        c += 1
+    first_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
+    first_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
+    first_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
+    first_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
+    
+    # Obtain the second column
+    i = 0
+    j = 1
+    while i < len(cipher_text_block):
+        second_column.append(cipher_text_block[i][j])
+        i += 1
 
-    first_column_result[0] = hex(int(hex(hex((int(mixColumn_matrix[0][0],16) * int(first_column[0],16))) ^ hex((int(mixColumn_matrix[0][1],16) * int(first_column[1],16))) ^ int(first_column[2],16) ^ int(first_column[3],16)),16) & 0xFF)[2:].zfill(2)
-    first_column_result[1] = hex(int(hex(int(first_column[0],16) ^ hex((int(mixColumn_matrix[1][1],16) * int(first_column[1],16))) ^ hex((int(mixColumn_matrix[1][2],16) * int(first_column[2],16))) ^ int(first_column[3],16)),16) & 0xFF)[2:].zfill(2)
-    first_column_result[2] = hex(int(hex(int(first_column[0],16) ^ int(first_column[1],16) ^ hex((int(mixColumn_matrix[2][2],16) * int(first_column[2],16))) ^ hex((int(mixColumn_matrix[2][3],16) * int(first_column[3],16)))),16) & 0xFF)[2:].zfill(2)
-    first_column_result[3] = hex(int(hex((int(mixColumn_matrix[3][0],16) * int(first_column[0],16))) ^ int(first_column[1],16) ^ int(first_column[2],16) ^ hex(int(mixColumn_matrix[3][3],16) * int(first_column[3],16)),16) & 0xFF)[2:].zfill(2)
+    c = 0
+    a = []
+    b = []
+    while c < 4:
+        a.append(hex(int(second_column[c],16)))
+        h = hex(int(second_column[c],16) & 0x80)
+        b.append(hex((int(second_column[c],16) << 1) % 256))
+        if (h == 0x80):
+            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
+        c += 1
+    second_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
+    second_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
+    second_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
+    second_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
     
-    print(first_column_result)
-    # i = 0
-    # j = 0
-    # counter = 0
-    # first_operator = 0
-    # second_operator = 0
-    # result = 0
-    # while counter < 4:
-    #     while i < len(cipher_text_block):
-    #         first_operator = cipher_text_block[i][j]
-    #         second_operator = hex(int(mixColumn_matrix[i][j]))[2:]
-    #         result = hex((int(first_operator, 16) * int(second_operator, 16)) & 0xFF)[2:].zfill(2)
-    #         cipher_text_block[i][j] = result
-    #         i += 1
-    #     j += 1
-    #     i = 0
-    #     counter += 1
+    # Obtain the thirth column
+    i = 0
+    j = 2
+    while i < len(cipher_text_block):
+        thirth_column.append(cipher_text_block[i][j])
+        i += 1
     
-    # print()
-    # print('Resultado tras el producto de columnas: ')
+    c = 0
+    a = []
+    b = []
+    while c < 4:
+        a.append(hex(int(thirth_column[c],16)))
+        h = hex(int(thirth_column[c],16) & 0x80)
+        b.append(hex((int(thirth_column[c],16) << 1) % 256))
+        if (h == 0x80):
+            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
+        c += 1
+    thirth_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
+    thirth_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
+    thirth_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
+    thirth_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
     
-    # i = 0
-    # j = 0
-    # while i < len(cipher_text_block):
-    #     while j < len(cipher_text_block[i]):
-    #         print(cipher_text_block[i][j], end=' ')
-    #         j += 1
-    #     print()
-    #     j = 0
-    #     i += 1
+    # Obtain the fourth column
+    i = 0
+    j = 3
+    while i < len(cipher_text_block):
+        fourth_column.append(cipher_text_block[i][j])
+        i += 1
     
-    # return cipher_text_block
+    c = 0
+    a = []
+    b = []
+    while c < 4:
+        a.append(hex(int(fourth_column[c],16)))
+        h = hex(int(fourth_column[c],16) & 0x80)
+        b.append(hex((int(fourth_column[c],16) << 1) % 256))
+        if (h == 0x80):
+            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
+        c += 1
+    fourth_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
+    fourth_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
+    fourth_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
+    fourth_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
+   
+    cipher_text_block_result = []
+   
+    cipher_text_block_result.append(first_column)
+    cipher_text_block_result.append(second_column)
+    cipher_text_block_result.append(thirth_column)
+    cipher_text_block_result.append(fourth_column)
+   
+    print()
+    print('Finalmente, tras la operación de MixColumns: ')
+    
+    print()
+    i = 0
+    j = 0
+    while i < len(cipher_text_block_result):
+        while j < len(cipher_text_block_result[i]):
+            print(cipher_text_block_result[i][j], end=' ')
+            j += 1
+        print()
+        j = 0
+        i += 1
