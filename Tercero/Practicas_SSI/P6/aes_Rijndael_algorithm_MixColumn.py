c28be39c6de95fef77c73c61cde55bd4efd4d48d
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-04-05 10:41:53 +0100

    Mixcolumn operation modificated

--- Tercero/Practicas_SSI/P6/aes_Rijndael_algorithm_MixColumn.py
+++ Tercero/Practicas_SSI/P6/aes_Rijndael_algorithm_MixColumn.py
@@ -8,42 +8,84 @@
  @copyright Copyright (c) 2022
 """
 
-# EL MIX COLUMN NO FUNCIONA DE MANERA CORRECTA
+from aes_Rijndael_algorithm_multiplication import AES_multplication
 
 # Multiply all the column to a standar matrix
 
 def MixColumn(cipher_text_block):
-    mixColumn_matrix = [['2','3','1','1'],     
-                        ['1','2','3','1'],
-                        ['1','1','2','3'],
-                        ['3','1','1','2']]
+    mixColumn_matrix = [['02','03','01','01'],     
+                        ['01','02','03','01'],
+                        ['01','01','02','03'],
+                        ['03','01','01','02']]
     
+    # This is for mixcolumn matrix
+    first_row = []
+    i = 0
+    j = 0
+    while j < len(mixColumn_matrix[i]):
+        first_row.append(mixColumn_matrix[i][j])
+        j += 1
+    
+    
+    second_row = []
+    i = 1
+    j = 0
+    while j < len(mixColumn_matrix[i]):
+        second_row.append(mixColumn_matrix[i][j])
+        j += 1
+    
+    thirth_row = []
+    i = 2
+    j = 0
+    while j < len(mixColumn_matrix[i]):
+        thirth_row.append(mixColumn_matrix[i][j])
+        j += 1
+   
+    fourth_row = []
+    i = 3
+    j = 0
+    while j < len(mixColumn_matrix[i]):
+        fourth_row.append(mixColumn_matrix[i][j])
+        j += 1
+    
+    # This is for cipher text block matrix
     first_column = []
-    second_column = []
-    thirth_column = []
-    fourth_column = []
-        
+    first_column_result = []
+    
     # Obtain the first column
     i = 0
     j = 0
     while i < len(cipher_text_block):
         first_column.append(cipher_text_block[i][j])
-        i += 1    
-    c = 0
-    a = []
-    b = []
-    while c < 4:
-        a.append(hex(int(first_column[c],16)))
-        h = hex(int(first_column[c],16) & 0x80)
-        b.append(hex((int(first_column[c],16) << 1) % 256))
-        if (h == 0x80):
-            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
-        c += 1
-    first_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
-    first_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
-    first_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
-    first_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
-    print(first_column)
+        i += 1
+   
+    first_operation = AES_multplication(first_row[0], first_column[0])
+    first_operation = hex(int(first_operation,2) & 0xFF)[2:].zfill(2)
+    second_operation = AES_multplication(first_row[1], first_column[1])
+    second_operation = hex(int(second_operation,2) & 0xFF)[2:].zfill(2)
+    first_column_result.append(hex(int(first_operation,16) ^ int(second_operation,16) ^ int(first_column[2],16) ^ int(first_column[3],16) & 0xFF)[2:].zfill(2))
+    first_operation = AES_multplication(second_row[1], first_column[1])
+    first_operation = hex(int(first_operation,2) & 0xFF)[2:].zfill(2)
+    print(first_operation)
+    second_operation = AES_multplication(second_row[2], first_column[2])
+    second_operation = hex(int(second_operation,2) & 0xFF)[2:].zfill(2)
+    print(second_operation)
+    first_column_result.append(hex(int(first_column[0],16) ^ int(first_operation,16) ^ int(second_operation,16) ^ int(first_column[3],16) & 0xFF)[2:].zfill(2))
+    first_operation = AES_multplication(thirth_row[2], first_column[2])
+    first_operation = hex(int(first_operation,2) & 0xFF)[2:].zfill(2)
+    second_operation = AES_multplication(thirth_row[3], first_column[3])
+    second_operation = hex(int(second_operation,2) & 0xFF)[2:].zfill(2)
+    first_column_result.append(hex(int(first_column[0],16) ^ int(first_column[1],16) ^ int(first_operation,16) ^ int(second_operation,16) & 0xFF)[2:].zfill(2))
+    first_operation = AES_multplication(fourth_row[0], first_column[0])
+    first_operation = hex(int(first_operation,2) & 0xFF)[2:].zfill(2)
+    second_operation = AES_multplication(fourth_row[3], first_column[3])
+    second_operation = hex(int(second_operation,2) & 0xFF)[2:].zfill(2)
+    first_column_result.append(hex(int(first_operation,16) ^ int(first_column[1],16) ^ int(first_column[2],16) ^ int(second_operation,16) & 0xFF)[2:].zfill(2))
+    
+    print(first_column_result)
+    
+    second_column = []
+    second_column_result = []
     # Obtain the second column
     i = 0
     j = 1
@@ -51,21 +93,8 @@ def MixColumn(cipher_text_block):
         second_column.append(cipher_text_block[i][j])
         i += 1
     
-    c = 0
-    a = []
-    b = []
-    while c < 4:
-        a.append(hex(int(second_column[c],16)))
-        h = hex(int(second_column[c],16) & 0x80)
-        b.append(hex((int(second_column[c],16) << 1) % 256))
-        if (h == 0x80):
-            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
-        c += 1
-    second_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
-    second_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
-    second_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
-    second_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
-    print(second_column)
+    thirth_column = []
+    thirth_column_result = [] 
     # Obtain the thirth column
     i = 0
     j = 2
@@ -73,21 +102,8 @@ def MixColumn(cipher_text_block):
         thirth_column.append(cipher_text_block[i][j])
         i += 1
     
-    c = 0
-    a = []
-    b = []
-    while c < 4:
-        a.append(hex(int(thirth_column[c],16)))
-        h = hex(int(thirth_column[c],16) & 0x80)
-        b.append(hex((int(thirth_column[c],16) << 1) % 256))
-        if (h == 0x80):
-            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
-        c += 1
-    thirth_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
-    thirth_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
-    thirth_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
-    thirth_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
-    print(thirth_column)
+    fourth_column = []
+    fourth_column_result = []
     # Obtain the fourth column
     i = 0
     j = 3
@@ -95,51 +111,3 @@ def MixColumn(cipher_text_block):
         fourth_column.append(cipher_text_block[i][j])
         i += 1
     
-    c = 0
-    a = []
-    b = []
-    while c < 4:
-        a.append(hex(int(fourth_column[c],16)))
-        h = hex(int(fourth_column[c],16) & 0x80)
-        b.append(hex((int(fourth_column[c],16) << 1) % 256))
-        if (h == 0x80):
-            b[c] = hex(int(int(b[c],16) ^ 0x1b,16))
-        c += 1
-    fourth_column[0] = hex(int(b[0],16) ^ int(a[3],16) ^ int(a[2],16) ^ int(b[1],16) ^ int(a[1],16) & 0xFF)[2:].zfill(2)
-    fourth_column[1] = hex(int(b[1],16) ^ int(a[0],16) ^ int(a[3],16) ^ int(b[2],16) ^ int(a[2],16) & 0xFF)[2:].zfill(2)
-    fourth_column[2] = hex(int(b[2],16) ^ int(a[1],16) ^ int(a[0],16) ^ int(b[3],16) ^ int(a[3],16) & 0xFF)[2:].zfill(2)
-    fourth_column[3] = hex(int(b[3],16) ^ int(a[2],16) ^ int(a[1],16) ^ int(b[0],16) ^ int(a[0],16) & 0xFF)[2:].zfill(2)
-    print(fourth_column)
-    auxiliary_row = []
-    cipher_text_block_result = []
-   
-    # To introduce the columns into the matrix
-    i = 0
-    while i < 4:
-        auxiliary_row.append(first_column[i])
-        auxiliary_row.append(second_column[i])
-        auxiliary_row.append(thirth_column[i])
-        auxiliary_row.append(fourth_column[i])
-        cipher_text_block_result.append(auxiliary_row)
-        auxiliary_row = []
-        i += 1
-   
-    # print()
-    # print('Finalmente, tras la operación de MixColumns: ')
-    
-    # print()
-    # i = 0
-    # j = 0
-    # while i < len(cipher_text_block_result):
-    #     while j < len(cipher_text_block_result[i]):
-    #         print(cipher_text_block_result[i][j], end=' ')
-    #         j += 1
-    #     print()
-    #     j = 0
-    #     i += 1
-        
-    print(cipher_text_block_result)
-    
-    return cipher_text_block_result
-
-# ESTO ESTÁ DE MANERA CORRECTA 
---
+++ Tercero/Practicas_SSI/P6/aes_Rijndael_algorithm_multiplication.py
@@ -0,0 +1,93 @@
+def AES_multplication(first_byte, second_byte):
+    # # Start
+    # print()
+    # print(f'El primer byte es: {first_byte}')
+    # print(f'El segundo byte es: {second_byte}')
+    # print('El algoritmo seleccionado es: AES')
+    # print()
+    
+    # Initialization
+    algorithm_byte = '00011011'
+    result = ''
+    result_aux = ''
+    for i in first_byte:
+        result = bin(int(i, 16))[2:].zfill(4)
+        result_aux = result_aux + result
+    first_byte = result_aux
+    print(first_byte)
+    
+    result = ''
+    result_aux = ''
+    for i in second_byte:
+        result = bin(int(i, 16))[2:].zfill(4)
+        result_aux = result_aux + result
+    second_byte = result_aux
+    print(second_byte)
+    
+    # # First exit
+    # print(f'El primer byte convertido es: {first_byte}')
+    # print(f'El segundo byte convertido es: {second_byte}')
+    # print(f'El byte del algoritmo es: {algorithm_byte}')
+    
+    # Table inicilization
+    # i = 0
+    # print()
+    # while i < 8:
+    #     print(f'{i}', end='            ');
+    #     i += 1
+    # print()
+    
+    # Multiplication algorithm
+    k = 1
+    l = 0
+    result = []
+    auxiliary_result = 0
+    # First iteration
+    table = first_byte
+    if second_byte[7] == '1':
+        result.append(table)
+    # Scrolling
+    if table[0] == '1': # Table iteration
+        table = bin((int(table, 2) << 1))[2:].zfill(8)
+        auxiliary = ''
+        j = 1
+        while j < len(table): # To obtain the first operator
+            auxiliary = auxiliary + table[j]
+            j += 1
+        table = auxiliary
+        auxiliary_result = bin(int(table, 2) ^ int(algorithm_byte, 2))[2:].zfill(8)
+        table = auxiliary_result
+    else:
+        table = bin(int(table, 2) << 1)[2:].zfill(8)
+    if second_byte[6] == '1':
+        result.append(table)
+    l = 5
+    while k < 7:
+        if table[0] == '1': # Table iteration
+            table = bin((int(table, 2) << 1))[2:].zfill(8)
+            auxiliary = ''
+            j = 1
+            while j < len(table): # To obtain the first operator
+                auxiliary = auxiliary + table[j]
+                j += 1
+            table = auxiliary
+            auxiliary_result = bin(int(table, 2) ^ int(algorithm_byte, 2))[2:].zfill(8)
+            table = auxiliary_result
+        else:
+            table = bin(int(table, 2) << 1)[2:].zfill(8)
+        if second_byte[l] == '1':
+            result.append(table)
+        l -= 1
+        k += 1
+        
+    # print()
+    m = 1
+    final_result = result[0]
+    while m < len(result):
+        final_result = bin(int(final_result, 2) ^ int(result[m], 2))[2:].zfill(8)
+        m += 1
+    # print()
+    # print(f'El resultado final de la multiplicación es: {final_result}')
+    # print()
+    
+    return final_result
